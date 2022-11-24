de45e75f93bcf7e810f9f11357d7c8bd0e585923
Yan-ni <anis01000001@gmail.com>
2022-11-25 00:50:49 +0100

    save history of network queries

--- network/server/database.py
+++ network/server/database.py
@@ -3,14 +3,28 @@ from psycopg.rows import dict_row
 from dotenv import load_dotenv
 from os import environ
 import json
+from datetime import datetime
 
 class Database:
 	def __init__(self, host, name, username, password):
 		# connect to the database
 		db_connection_string = f'host={host} dbname={name} user={username} password={password}'
-		db_connection = psycopg.connect(db_connection_string)
-		self.cur = db_connection.cursor(row_factory=dict_row)
+		self.db_connection = psycopg.connect(db_connection_string)
+		self.cur = self.db_connection.cursor(row_factory=dict_row)
 
+	def save_history(func):
+		def wrapper(self, *args):
+			val = func(self, *args)
+			if val != 0 and val < 3:
+				return val
+			numero_carte, numero_porte = args
+			date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+			self.cur.execute('INSERT INTO carte_porte VALUES (%s, %s, %s, %s)', (numero_carte, numero_porte, date, val))
+			self.db_connection.commit()
+			return val
+		return wrapper
+
+	@save_history
 	def verifier(self, numero_carte: str, numero_porte:str) -> int:
 		self.cur.execute('SELECT * FROM carte WHERE numero_carte = %s', (numero_carte,))
 		carte = self.cur.fetchone()
